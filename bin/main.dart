89e78d8506412128d74911ce158ad3d5b8519c3b
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-07 01:28:49 +0330

    abstract method descriptions

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,42 @@
 void main() {
-  var names = 'ali mamad reza';
+  PetrolEngine().start();
+  DieselEngine().start();
+  Platform().start();
+  BenzS500();
+}
 
-  var splitNames = names.split(' ');
-  print(splitNames);
+abstract class Engine {
+  int cylinder = 4;
+  late int horsePower;
+  void start();
+}
+
+class PetrolEngine extends Engine {
+  @override
+  void start() {
+    print('Petrol engine start...');
+  }
+}
+
+class DieselEngine extends Engine {
+  @override
+  void start() {
+    print('Diesel engine start...');
+  }
+}
+
+class Platform extends PetrolEngine {
+  @override
+  void start() {
+    print('This car has air conditioner');
+    super.start();
+  }
+}
+
+class BenzS500 extends Platform {
+  BenzS500() {
+    horsePower = 400;
+    print('horse power is $horsePower');
+    print('cylinders are $cylinder');
+  }
 }
