b176ea78a066caa241a9765c1a7974c0b1fd1df4
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-15 00:57:58 +0330

    http and future exp

--- flutter_app/flutter_application_1/lib/http.dart
+++ flutter_app/flutter_application_1/lib/http.dart
@@ -1,21 +1,21 @@
-// import 'dart:convert';
+// // import 'dart:convert';
 
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
-void main(List<String> args) async {
-  http.Response response = await priceUsd(
-    'free3HSIkITicxJLc7c38A98JrEYslzu',
-    'usd_buy',
-  );
+// void main(List<String> args) async {
+//   http.Response response = await priceUsd(
+//     'free3HSIkITicxJLc7c38A98JrEYslzu',
+//     'usd_buy',
+//   );
 
-  print(response.body);
-}
+//   print(response.body);
+// }
 
-Future<http.Response> priceUsd(String apiKey, String item) {
-  return http.post(
-    Uri.parse('https://pars-string.free.beeceptor.com'),
-    headers: {"accept-encoding": "iso"},
-    // Uri.parse('http://api.navasan.tech/latest/?api_key=$apiKey&item=$item'),
-    //  body: jsonEncode({'api_key': 'free3HSIkITicxJLc7c38A98JrEYslzu'}),
-  );
-}
+// Future<http.Response> priceUsd(String apiKey, String item) {
+//   return http.post(
+//     Uri.parse('https://pars-string.free.beeceptor.com'),
+//     headers: {"accept-encoding": "iso"},
+//     // Uri.parse('http://api.navasan.tech/latest/?api_key=$apiKey&item=$item'),
+//     //  body: jsonEncode({'api_key': 'free3HSIkITicxJLc7c38A98JrEYslzu'}),
+//   );
+// }
--- flutter_app/flutter_application_1/lib/main.dart
+++ flutter_app/flutter_application_1/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-import 'integer_stream.dart';
+// import 'integer_stream.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,19 +12,58 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future dollarPrice() {
+    return Future.delayed(
+      const Duration(seconds: 2),
+      () => dollarAPI(),
+    );
+  }
+
+  String arrangeMap(Map theMap, String searchText) {
+    for (var thisJson in theMap.values) {
+      return thisJson[searchText];
+    }
+    return 'Not Found';
+  }
+
+  Future dollarAPI() async {
+    final http.Response response = await http.post(
+      Uri.parse(
+          'http://api.navasan.tech/latest/?api_key=free3HSIkITicxJLc7c38A98JrEYslzu&item=usd_buy'),
+    );
+    var json = jsonDecode(response.body);
+    return arrangeMap(json, 'value');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: StreamBuilder(
-      initialData: 'wait...',
-      stream: IntegerStream().stream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text(snapshot.data as String);
-        } else {
-          return Text('Error: ${snapshot.error}');
-        }
-      },
-    ));
+    return MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: FutureBuilder(
+          future: dollarPrice(),
+          initialData: 'Waiting...',
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data as String);
+            } else {
+              return Text(snapshot.error as String);
+            }
+          },
+        ),
+      ),
+    )
+        //   StreamBuilder(
+        // initialData: 'wait...',
+        // stream: IntegerStream().stream,
+        // builder: (context, snapshot) {
+        //   if (snapshot.hasData) {
+        //     return Text(snapshot.data as String);
+        //   } else {
+        //     return Text('Error: ${snapshot.error}');
+        //   }
+        // },
+        // )
+        );
   }
 }
