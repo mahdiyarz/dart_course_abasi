0f9ae55cf77532329e83c3aa4feaae0f3695ee03
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-01-14 21:47:41 +0330

    Use beeceptor site for api

--- flutter_app/flutter_application_1/lib/http.dart
+++ flutter_app/flutter_application_1/lib/http.dart
@@ -1,3 +1,5 @@
+// import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 void main(List<String> args) async {
@@ -11,7 +13,9 @@ void main(List<String> args) async {
 
 Future<http.Response> priceUsd(String apiKey, String item) {
   return http.post(
-    Uri.parse('http://api.navasan.tech/latest/?api_key=$apiKey&item=$item'),
-    //! body: jsonEncode({'api_key': 'free3HSIkITicxJLc7c38A98JrEYslzu'}),
+    Uri.parse('https://pars-string.free.beeceptor.com'),
+    headers: {"accept-encoding": "iso"},
+    // Uri.parse('http://api.navasan.tech/latest/?api_key=$apiKey&item=$item'),
+    //  body: jsonEncode({'api_key': 'free3HSIkITicxJLc7c38A98JrEYslzu'}),
   );
 }
